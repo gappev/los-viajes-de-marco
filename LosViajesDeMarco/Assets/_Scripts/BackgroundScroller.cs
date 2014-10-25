bb73101115a7727fb2cb02510183a030998218d2
Oscar Rene Presidente <oscar.rene1989@gmail.com>
2014-10-25 17:32:28 -0600

    Easy Touch Plugin

--- LosViajesDeMarco/Assets/_Scripts/BackgroundScroller.cs
+++ LosViajesDeMarco/Assets/_Scripts/BackgroundScroller.cs
@@ -9,14 +9,8 @@ public class BackgroundScroller : MonoBehaviour {
 	void Update () {
 		Vector3 position = transform.position;
 		float movement = scrollingSpeed * Time.deltaTime;
-		//transform.Translate(-Vector2.up * scrollingSpeed * Time.deltaTime);
 		position.y = (position.y - movement);
-
-		if ( position.y < -14.0f)
-			position.y = 0.0f;
-
+		position.y = position.y % 14;
 		transform.position = position;
-
-		Debug.Log(position);
 	}
 }
